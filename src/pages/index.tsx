e023f5131382ca2055b9bfe23f7d210333af823a
Felipe Fadoni <felipe.fadoni@gsw.com.br>
2021-07-27 19:07:13 -0300

    configuracao home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+import { SubscribeButton } from "../components/SubscribeButton";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Home | IgNews</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />{" "}
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
