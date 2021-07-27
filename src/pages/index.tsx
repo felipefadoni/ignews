f9a2f85b72b3499e9c385aee4caa48b7b5f551b1
Felipe Fadoni <felipe.fadoni@gsw.com.br>
2021-07-27 19:34:54 -0300

    get dados product stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
 import styles from "./home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,10 +27,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />{" "}
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -29,3 +38,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JHyRkIyBkuECldkI79LaVGX");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
